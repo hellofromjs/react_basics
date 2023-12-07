b38108d176885daa3419e3fa165107d7352a5b71
Edvinas <edvinas.gudavicius@stud.kitm.lt>
2023-12-07 11:20:18 +0200

    init

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import Header from '../header/Header';
+import Main from '../main/Main';
+import Footer from '../footer/Footer';
+
+const App = () => {
+  return (
+    <div className="App">
+     <Header/>
+     <Main/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main/Main.js
@@ -0,0 +1,17 @@
+import Article from "../article/Article"
+
+const Main = () => {
+    return (
+        <main>
+            <div className="container">
+                <h1>Naujienos</h1>
+                <Article/>
+                <Article/>
+                <Article/>
+                <Article/>
+            </div>
+        </main>
+    )
+}
+
+export default Main
